74d62f82d1b8e150c0a693d033baf6c5bf0d0b8f
Leandro Alves <leandro.job@live.com>
2016-05-18 19:11:23 -0300

    Troubleshoot Protractor

--- config/protractor.js
+++ config/protractor.js
@@ -12,7 +12,6 @@ exports.config = {
 	specs: ['../test/e2e/**/*.js'],
 	onPrepare: function(){
 		browser.driver.get('http://localhost:3000');
-		browser.debugger();
 		browser.driver.findElement(by.id('entrar')).click();
 		browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
 		browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
