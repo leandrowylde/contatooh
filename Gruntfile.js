e0f45554684059bccfa1d40fcb102cf221f8bbe6
Leandro Alves <Wylde@Wylde.local>
2016-07-31 18:35:40 -0300

    Commit Deploy Openshift

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt){
 
 	grunt.initConfig({
-		copy: {
+	/*	copy: {
 			project: {
 				expand: true,
 				cwd: '.',
@@ -13,33 +13,33 @@ module.exports = function(grunt){
 			dist: {
 				src: 'dist'
 			}
-		},
+		},*/
 		usemin: {
-			html: 'dist/app/views/**/*.ejs'
+			html: 'app/views/**/*.ejs'
 		},
 		useminPrepare: {
 			options: {
-				root: 'dist/public',
-				dest: 'dist/public'	
+				root: 'public',
+				dest: 'public'	
 			},
-			html: 'dist/app/views/**/*.ejs'
+			html: 'app/views/**/*.ejs'
 		},
 		ngAnnotate: {
 			scripts: {
 				expand: true,
-				src: [ 'dist/public/js/**/*.js']
+				src: [ 'public/js/**/*.js']
 			}
 		}
 	});
 
 
 	grunt.registerTask('minifica', ['useminPrepare', 'ngAnnotate', 'concat', 'uglify', 'cssmin', 'usemin']);
-	grunt.registerTask('default', ['dist', 'minifica']);
-	grunt.registerTask('dist', ['clean', 'copy']);
+/*	grunt.registerTask('default', ['dist', 'minifica']);
+	grunt.registerTask('dist', ['clean', 'copy']);*/
 
 	grunt.loadNpmTasks('grunt-ng-annotate');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-contrib-clean');
+	/*grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');*/
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
